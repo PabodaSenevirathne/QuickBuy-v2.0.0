7745f8810102e5455cc6178930e389ec503bbf7d
Paboda Senevirathne <pabodasenevirathne@gmail.com>
2024-04-19 09:14:45 -0400

    bug fixed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,18 +23,7 @@ function Home() {
   return (
     <div>
       <CustomCarousel />
-      {/* <h2 className="section-title">Best Sellers</h2>
-      <div className="row">
-        <div className="col">
-          <section className="cards">
-            {products.map((product) => (
-              <Product key={product.productId} product={product} />
-            ))}
-          </section>
-        </div>
-      </div> */}
-
-<h2 className="section-title">Best Sellers</h2>
+      <h2 className="section-title">Best Sellers</h2>
       <div className="row">
         <div className="col">
           <section className="cards">
@@ -59,51 +48,3 @@ function Home() {
 }
 
 export default Home;
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import Product from '../components/Product';
-// import CustomCarousel from '../components/CustomCarousel';
-
-// import '../styles/Home.css';
-
-// function Home({ products, addToCart }) {
-
-//   const productsRow1 = products.slice(0, Math.ceil(products.length / 2));
-//   const productsRow2 = products.slice(Math.ceil(products.length / 2));
-
-//   return (
-//     <div>
-//         <CustomCarousel />
-//         <h2 className="section-title">Best Sellers</h2>
-//         <div className="row">
-//           <div className="col">
-//             <section className="cards">
-//               {productsRow1.map((product) => (
-//                 <Product key={product.id} product={product} />
-//               ))}
-//             </section>
-//           </div>
-//           <h2 className="section-title">New Arrivals</h2>
-//           <div className="col">
-//             <section className="cards">
-//               {productsRow2.map((product) => (
-//                 <Product key={product.id} product={product} />
-//               ))}
-//             </section>
-//           </div>
-//         </div>
-//     </div>
-//   );
-// }
-
-// export default Home;
